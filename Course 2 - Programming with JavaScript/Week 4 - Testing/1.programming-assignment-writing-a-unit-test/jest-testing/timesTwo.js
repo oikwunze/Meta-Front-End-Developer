26f8f0c2bb5456ae47759bf6dbfca04d3d6544c5
Ginny-Dang <ginnydang0728@gmail.com>
2022-08-23 17:54:24 -0500

    Update Week 4 of Course 2 with unit testing assignment

---
+++ Course 2 - Programming with JavaScript/Week 4 - Testing/1.programming-assignment-writing-a-unit-test/jest-testing/timesTwo.js
@@ -0,0 +1,7 @@
+// Task 1: Code the timesTwo function declaration
+function timesTwo(number) {
+    return 2 * number;
+}
+
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
